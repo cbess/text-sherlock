c05f429fd06e5f0126ea2ebd6c4ab2b1495d6194
Christopher Bess <christopher.bess@bottlerocketapps.com>
2011-10-04 17:47:50 -0500

    updated local settings example

--- example.local_settings.py
+++ example.local_settings.py
@@ -2,6 +2,11 @@
 # Make a copy of this file and rename it to `local_settings.py`
 # DO NOT import `settings.py` into this script, it will cause a circular import
 
+# A value indicating whether the app runs in debug mode.
+# type: boolean
+# default: True (set to False for production or in untrusted environments use)
+DEBUG = False
+
 # During the indexing all items with the given suffix will be exclude from the index.
 # Only checks filenames, for now.
 # type: tuple
