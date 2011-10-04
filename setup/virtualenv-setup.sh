90f43690be0b6ebee2de6f1c4ebe73b454cfcee0
Christopher Bess <christopher.bess@bottlerocketapps.com>
2011-10-04 17:46:58 -0500

    added more logging during installation; removed flask-peewee pip
    - enabled pip and virtualenv install, by default

--- setup/virtualenv-setup.sh
+++ setup/virtualenv-setup.sh
@@ -1,13 +1,20 @@
 # global
 # Created by: Christopher Bess
 # if no pip or virtualenv, uncomment the two lines below
-#sudo easy_install pip
-#sudo pip install virtualenv
+
+echo "Setting up virtualenv and pip"
+sudo easy_install pip
+sudo pip install virtualenv
 
 # local
 # if on a Mac exec below line (maybe)
 # ARCHFLAGS="-arch i386 -arch x86_64"
-mkdir ../data && ../data/indexes
+mkdir ../data
+mkdir ../data/indexes
 virtualenv ../sherlock_env --distribute --no-site-packages
 source ../sherlock_env/bin/activate
+
+echo "Installing sherlock dependencies"
 pip install -r requirements.txt
+
+echo "Done, sherlock install finished"
