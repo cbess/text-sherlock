f7ed979d53e331c6399dc3a185f3ca4986b2c503
C. Bess <Qu@qumacbookair.local>
2011-10-08 01:07:42 -0500

    - updated xapian install scripts

--- setup/install-xapian.sh
+++ setup/install-xapian.sh
@@ -3,7 +3,6 @@
 
 echo "Installing xapian"
 # xapian support
-# or http://xappy.googlecode.com/files/xapian-core-14622.tar.gz
 curl -o xapian.tar.gz http://oligarchy.co.uk/xapian/1.2.7/xapian-core-1.2.7.tar.gz
 tar -xvzf xapian.tar.gz
 cd xapian*/
@@ -15,7 +14,6 @@ cd ..
 
 echo "Installing xapian python bindings"
 # bindings
-# or http://xappy.googlecode.com/files/xapian-bindings-14622.tar.gz
 curl -o xapian-bindings.tar.gz http://oligarchy.co.uk/xapian/1.2.7/xapian-bindings-1.2.7.tar.gz
 tar -xvzf xapian-bindings.tar.gz
 cd xapian-bindings*/
